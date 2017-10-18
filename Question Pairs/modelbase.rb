31c3019603bd7aa3391ace11ba2996231d609de7
App Academy Student <student@appacademy.io>
2017-10-17 17:57:45 -0700

    Develop find_by

--- Question Pairs/modelbase.rb
+++ Question Pairs/modelbase.rb
@@ -22,9 +22,13 @@ class ModelBase
   end
 
   def self.where(options)
-    wheres = []
-    options.each { |k, v| wheres << "#{k} = '#{v}'" }
-    wheres = wheres.join(" AND ")
+    if options.is_a?(Hash)
+      wheres = []
+      options.each { |k, v| wheres << "#{k} = '#{v}'" }
+      wheres = wheres.join(" AND ")
+    else
+      wheres = options
+    end
     data = QuestionsDatabase.instance.execute(<<-SQL)
       SELECT
         *
@@ -36,4 +40,23 @@ class ModelBase
     data.map { |datum| self.new(datum) }
   end
 
+
+  def method_missing(method_name, *args)
+    puts 'loop'
+    method_name = method_name.to_s
+    if method_name.start_with?("find_by_")
+      attributes_string = method_name[("find_by_".length)..-1]
+      attribute_names = attributes_string.split("_and_")
+      unless attribute_names.length == args.length
+        raise "unexpected # of arguments"
+      end
+      search_conditions = {}
+      attribute_names.each_index do |i|
+        search_conditions[attribute_names[i]] = args[i]
+      end
+      self.where(search_conditions)
+    else
+      super
+    end
+  end
 end
